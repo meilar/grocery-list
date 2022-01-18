c38b381de8c5956aed28f61877a031c9b1288efb
Ryan Ashby <rjapsu160@gmail.com>
2022-01-18 13:38:43 -0800

    add function to push to list, remove form, and show list
    Co-authored-by: Matt Eilar <meilar@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    let list = []
+    list.push($("input#item1").val());
+    list.push($("input#item2").val());
+    list.push($("input#item3").val());
+    list.push($("input#item4").val());
+    list.push($("input#item5").val());
+    $("form").hide();
+
+    let upperCaseList = []
+
+    list.sort().forEach(function(item) {
+      upperCaseList.push(item.toString().toUpperCase());
+    });
+
+    console.log(upperCaseList);
+    console.log(typeof upperCaseList);
+    upperCaseList.forEach(function(item) {
+    $("ul#gro-list").append("<li>" + item + "</li")
+    });
+  });
+});
